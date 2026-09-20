b4575a48d7ea9ccb2828ba7ac56765f80e9f05d4
AzifAliZ <azifalizakir@gmail.com>
2026-09-20 13:19:43 +0530

    Configure Telegram webhook

--- app/web_server.py
+++ app/web_server.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI, Request
 from telegram import Update
-
+import os
 from app.webhook import application
 
 
 app = FastAPI()
 
 
+
+
+
 @app.on_event("startup")
 async def startup():
     await application.initialize()
+
+    render_url = os.getenv("RENDER_EXTERNAL_URL")
+
+    if render_url:
+        await application.bot.set_webhook(
+            url=f"{render_url}/webhook"
+        )
 
 
 @app.on_event("shutdown")
